e70976e2bbc0e07f809fa757e57ec344e0a740bb
oweskee <owen_m_scott@comcast.net>
2015-08-03 14:48:52 -0400

    8/3/15

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    var randomizer = $('.slides');
+    var count = $('.slides li').length;
+    for (var i = 1; i < count; i++) {
+        var j = Math.floor(Math.random() * count);
+        $('li', randomizer).eq(j).appendTo(randomizer);
+    }
+
+
+   $('.slider').slider({full_width:true, transition:2000, interval:4000});
+
+   $(".button-collapse").sideNav();
+
+   $('a.animated.flipInX').on('click',function(){
+
+     $('a.animated.flipInX').addClass('slideOutRight');
+
+   });
+
+ });
